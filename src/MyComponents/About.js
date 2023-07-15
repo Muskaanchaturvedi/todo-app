62200451d726ab554bc8f3afb2f3fe3e15ebd6c5
Muskaan <muskaanchaturvedi111@gmail.com>
2023-07-15 09:14:19 +0530

    added relevant files

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <>
+    <div>This is an about component</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, blanditiis. Possimus quisquam ab error quibusdam qui? Similique cupiditate fuga reiciendis magnam temporibus. Ab optio sed, est assumenda culpa dignissimos eveniet quis eaque aut iusto?</p>
+    </>
+
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { About } from './MyComponents/About';
+import { Home } from './MyComponents/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/about', element: <About /> },
+    ],
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
